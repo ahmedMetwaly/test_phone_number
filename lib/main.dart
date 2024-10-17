2115d8a12277d4ae865afa1bb5531b0a087c758e
Ahmed Metwaly <ahmed10metwaly99@gmail.com>
2024-10-18 02:29:12 +0300

    add phone

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_phone_number/phone.dart';
 
 void main() {
   runApp(const MyApp());
@@ -112,6 +113,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            Phone(controller: TextEditingController())
           ],
         ),
       ),
